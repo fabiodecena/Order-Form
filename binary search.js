8de54a8dfcb1ac051da82333b499d411ad87958a
Fabio <fabiodecena@gmail.com>
2023-10-02 12:32:04 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   binary search.js

--- binary search.js
+++ binary search.js
@@ -1,20 +1,19 @@
 function binarySearch(arr,val) {      // function declaration.     
-    let start = 0, end = arr.length-1, found = 'Value not found!';     // variable declaration and assignement: assigned zero (first) index of the list to start, the last index of the list to end and the appropriate default value to found (could be also -1).
+    let start = 0, end = arr.length-1;     // variable declaration and assignement: assigned zero (first) index of the list to start and the last index of the list to end. 
     while (start <= end) {      // set a while loop that iterate until the start var is not 'crossing' the end of the list.
-        let mid = Math.floor((start + end) / 2)  //set mid var as the middle index of the list (floor division)
-        if (arr[mid] == val) {
-            found = mid;
-            break
-        }else {
-            if(val < arr[mid]) {
+        let mid = Math.floor((start + end) / 2)  //set mid var as the middle index of the list (floor division).
+        if (arr[mid] == val) {     // if the value of the middle index is equal to the value we are looking for, return the index of the value.
+            return 'Value found at index: ' + mid;
+        }else {     // if the value of the middle index is not equal to the value we are looking for, check if the value is smaller or bigger than the value of the middle index.                        
+            if(val < arr[mid]) { // if the value is smaller, set the end var to the index before the middle index.
                 end = mid - 1;
-            }else {
+            }else { // if the value is bigger, set the start var to the index after the middle index.
                 start = mid + 1
             }
         }
     }
-    return found
+    return 'Value not found!' // if the value is not found, return this message.
 }
 
 let myList = [-23, -12, -5, 1, 7, 24, 56, 99] // list have to be sorted!
-console.log(binarySearch(myList, 24))
+console.log(binarySearch(myList, 24)) // call the function and print the result to the console.
